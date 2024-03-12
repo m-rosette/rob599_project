8cd2a4015888db1951e5fde1d1fa7fd6e1fa21fa
Marcus <rosettem@oregonstate.edu>
2024-03-11 17:58:47 -0700

    making standalone dynamixel control instead of a Dynamixel sub-repo

---
+++ dynamixel_control/dynamixel_control/include/motor_interface.hpp
@@ -0,0 +1,36 @@
+#ifndef READ_WRITE_NODE_HPP_
+#define READ_WRITE_NODE_HPP_
+
+#include <cstdio>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "rcutils/cmdline_parser.h"
+#include "dynamixel_sdk/dynamixel_sdk.h"
+#include "dynamixel_sdk_custom_interfaces/msg/set_position.hpp"
+#include "dynamixel_control_msgs/srv/set_operating_mode.hpp"
+
+class ReadWriteNode : public rclcpp::Node
+{
+public:
+  using SetPosition = dynamixel_sdk_custom_interfaces::msg::SetPosition;
+  using GetPosition = dynamixel_sdk_custom_interfaces::srv::GetPosition;
+  using SetOperatingMode = dynamixel_control_msgs::srv::SetOperatingMode;
+
+  ReadWriteNode();
+  virtual ~ReadWriteNode();
+
+private:
+  void updateGoalCurrent();
+
+  rclcpp::Subscription<SetPosition>::SharedPtr set_position_subscriber_;
+  rclcpp::Service<SetOperatingMode>::SharedPtr set_operating_mode_service_;
+  rclcpp::Service<GetPosition>::SharedPtr get_position_server_;
+
+  int present_position;
+  int goal_current;
+  // rclcpp::TimerBase::SharedPtr goal_current_timer;
+};
+
+#endif  // READ_WRITE_NODE_HPP_
