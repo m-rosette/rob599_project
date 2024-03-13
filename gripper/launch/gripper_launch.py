4232a4252e0fd4710bf451db6287f447eaed8357
Karina Puente <puentek@oregonstate.edu>
2024-03-13 12:21:27 -0700

    launch file edit test

--- gripper/launch/gripper_launch.py
+++ gripper/launch/gripper_launch.py
@@ -1,1 +1,10 @@
 #  launch file for the linear actuator, dynamixels,and tactile sensors 
+#!/usr/bin/env python3
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
