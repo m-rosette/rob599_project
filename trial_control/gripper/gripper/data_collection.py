6be0812deb5191d72a4568b068a8c31c14a2b6ca
Marcus <rosettem@oregonstate.edu>
2024-03-18 12:25:22 -0700

    building proper clients for main control

--- trial_control/gripper/gripper/data_collection.py
+++ trial_control/gripper/gripper/data_collection.py
@@ -12,6 +12,24 @@ from gripper_msgs.srv import LinearActuator
 from dynamixel_control_msgs.srv import DualSetOperatingMode
 
 
+# Linear actuator positions (value: negative = forward, positive = backward)
+BACKWARD_POS = 0
+FORWARD_POS = -10000
+
+# Dynamixel current (torque) variables
+CURRENT_MODE = 0
+CLOSE_CURRENT = 65
+UPPER_CURRENT_BOUND = 100
+LOWER_CURRENT_BOUND = 10
+
+# Dynamixel position variables
+POSITION_MODE = 3
+UPPER_POS_BOUND = 500
+LOWER_POS_BOUND = 50
+MAX_POS = 4075
+FULLY_OPEN_POS = 50
+
+
 class GripperControl(Node):
     def __init__(self):
         # Initialize the superclass
@@ -25,55 +43,135 @@ class GripperControl(Node):
         # Wait until the server is ready to accept an action request
         self.record_client.wait_for_server()
 
-        # Create service clients
+        # Create dynamixel service client
         self.dynamixel_client = self.create_client(DualSetOperatingMode, 'set_operating_mode')
+        
+        # Wait until we have a connection to the server.
+        while not self.dynamixel_client.wait_for_service(timeout_sec=1):
+            self.get_logger().info('waiting for dynamixel service to start')
+
+        # Create linear actuator service client
         self.linear_actuator_client = self.create_client(LinearActuator, 'input_number')
+
+        # Wait until we have a connection to the server.
+        while not self.dynamixel_client.wait_for_service(timeout_sec=1):
+            self.get_logger().info('waiting for dynamixel service to start')
+    
+    def save_tactile_data(self, filename):
+        # Log the status of the user input
+        self.get_logger().info(f"Saving data to filename: {filename}")
+
+        # Get instance of action goal
+        goal = RecordData.Goal()
+        goal.filename = filename
+
+        # Send goal
+        self.record_client.send_goal_async(goal)
+
+    def send_linear_actuator_request(self, target_position):
+        self.get_logger().info(f"Requested linear actuator position: {target_position}")
+
+        # Get instance of srv
+        request = LinearActuator.Request()
+        request.location_goal = target_position
+
+        # Send request
+        self.response = self.linear_actuator_client.call_async(request)
+
+    def send_dual_motor_request(self, operating_mode, operation_target):
+        # Get instance of srv
+        request = DualSetOperatingMode.Request()
+        request.id1 = 1
+        request.id2 = 2
+        request.operating_mode = operating_mode
+
+        bounded_operation_target = self.bound_dual_motor_input(operating_mode, operation_target)
+        inverted_operation_target = self.invert_second_motor(operating_mode, bounded_operation_target)
+
+        self.get_logger().info(f"Dynamixel Request: [Operating mode: {operating_mode}] [Operation target: {bounded_operation_target}]")
+
+        request.operation_target1 = bounded_operation_target
+        request.operation_target2 = inverted_operation_target
+
+        # Send request
+        self.response = self.dynamixel_client.call_async(request)
+
+    def bound_dual_motor_input(self, operating_mode, operation_target):
+        # Check the input position bounds
+        if operating_mode == POSITION_MODE and operation_target > UPPER_POS_BOUND:
+            self.get_logger().warn(F"Position upper bound exceeded - capping value. [Goal Position: {UPPER_POS_BOUND}]")
+            operation_target = UPPER_POS_BOUND
+        elif operating_mode == POSITION_MODE and operation_target < LOWER_POS_BOUND:
+            self.get_logger().warn(F"Position lower bound exceeded - capping value. [Goal Position: {LOWER_POS_BOUND}]")
+            operation_target = LOWER_POS_BOUND
+
+        # Check the input current bounds
+        if operating_mode == CURRENT_MODE and operation_target > UPPER_CURRENT_BOUND:
+            self.get_logger().warn(F"Current upper bound exceeded - capping value. [Goal Current: {UPPER_CURRENT_BOUND}]")
+            operation_target = UPPER_CURRENT_BOUND
+        elif operating_mode == CURRENT_MODE and operation_target < LOWER_CURRENT_BOUND:
+            self.get_logger().warn(F"Current lower bound exceeded - capping value. [Goal Current: {LOWER_CURRENT_BOUND}]")
+            operation_target = LOWER_CURRENT_BOUND
+
+        return operation_target
+
+    def invert_second_motor(self, operating_mode, operation_target):
+        # Invert position
+        if operating_mode == POSITION_MODE:
+            inverted_operation_target = MAX_POS - operation_target
+        
+        # Invert current
+        if operating_mode == CURRENT_MODE:
+            inverted_operation_target = - operation_target
+
+        return inverted_operation_target
 
 
 def main(args=None):
     # Initialize rclpy
     rclpy.init(args=args)
 
+    # Make a node class
+    gripper_control = GripperControl()
+
     # get user input filename
     try:
         filename = str(sys.argv[1])
     except:
-        filename = "None"
-
-    # Make a node class
-    gripper_control = GripperControl()    
-
-    # Log the status of the user input
-    gripper_control.get_logger().info(f"Saving data to filename: {filename}")
+        # If no filename is given or too many command line inputs 
+        if len(sys.argv) != 2:
+            gripper_control.get_logger().error("Usage: ros2 run data_collection <filename>")
+            rclpy.shutdown()
+            return 
 
     # Send the filename to the record action (starts recording)
-    goal = RecordData.Goal()
-    goal.filename = filename
-    gripper_control.record_client.send_goal_async(goal)
+    gripper_control.save_tactile_data(filename)
 
     # Move linear actuator forward
-    desired_position = -5000
-    linear_actuator_command = 'ros2 service call /input_number gripper_msgs/srv/LinearActuator "{location_goal: -5000}"'
-    os.system(linear_actuator_command)
-    time.sleep(10)
+    gripper_control.get_logger().info("Moving linear actuator forward")
+    gripper_control.send_linear_actuator_request(FORWARD_POS)
+
+    # Allow time for the linear actuator to move
+    time.sleep(11)
 
     # Close gripper (current mode)
-    dynamixel_command = "ros2 run dynamixel_control dual_motor_client 0 65"
-    os.system(dynamixel_command)
+    gripper_control.get_logger().info("Closing gripper")
+    gripper_control.send_dual_motor_request(CURRENT_MODE, CLOSE_CURRENT)
 
-    # Move linear actuator forward
-    desired_position = 5000
-    linear_actuator_command = 'ros2 service call /input_number gripper_msgs/srv/LinearActuator "{location_goal: 5000}"'
-    os.system(linear_actuator_command)
-    time.sleep(10)
+    # Move linear actuator backward
+    gripper_control.get_logger().info("Moving linear actuator backward")
+    gripper_control.send_linear_actuator_request(BACKWARD_POS)
 
-    # Close gripper (position mode)
-    dynamixel_command = "ros2 run dynamixel_control dual_motor_client 3 50"
-    os.system(dynamixel_command)
+    # Allow time for the linear actuator to move
+    time.sleep(11)
 
+    # Open gripper (position mode)
+    gripper_control.get_logger().info("Opening gripper")
+    gripper_control.send_dual_motor_request(POSITION_MODE, FULLY_OPEN_POS)
 
     # Shutdown everything cleanly
     rclpy.shutdown()
+
 
     if __name__ == '__main__':
         main()
